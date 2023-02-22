5b3673c23269945ed332d7422f4cba57cafad1fd
sultan <hzqsultan@qq.com>
2023-02-22 14:15:31 +0800

    hanlp/mynlp测试

--- src/test/java/com/hzq/search/ResearchTestApplicationTests.java
+++ src/test/java/com/hzq/search/ResearchTestApplicationTests.java
@@ -3,6 +3,8 @@ package com.hzq.search;
 import com.hankcs.hanlp.HanLP;
 import com.hankcs.hanlp.dictionary.py.Pinyin;
 import com.hankcs.hanlp.seg.common.Term;
+import com.mayabot.nlp.module.pinyin.Pinyins;
+import com.mayabot.nlp.module.pinyin.split.PinyinSplits;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -16,7 +18,16 @@ public class ResearchTestApplicationTests {
 
     @Test
     public void contextLoads() {
-        List<Term> woaibeijingtiananmen = HanLP.segment("啊沙发沙发地方");
-        System.out.println(woaibeijingtiananmen);
+        List<Term> s1 = HanLP.segment("啊沙发沙发地方");
+        System.out.println(s1);
+
+        List<String> s2 = PinyinSplits.split("woaibeijintiananmen");
+        System.out.println(s2);
+
+
+
+        List<String> s3 = PinyinSplits.split("womende提案haobuhao");
+        System.out.println(s3);
+
     }
 }
