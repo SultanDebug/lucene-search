7e357dc62d0d4e9eee9409d4756e7d1263476a3a
sultan <hzqsultan@qq.com>
2023-03-14 16:55:56 +0800

    节点状态查询返回优化

--- src/main/java/com/hzq/search/service/shard/IndexInfoService.java
+++ src/main/java/com/hzq/search/service/shard/IndexInfoService.java
@@ -21,49 +21,49 @@ public class IndexInfoService extends IndexCommonAbstract {
     @Resource
     public IndexConfig indexConfig;
 
-    public void getCurIndex(String index, Map<String,String> map){
+    public void getCurIndex(String index, Map<String, String> map) {
         Map<String, IndexShardConfig> indexMap = indexConfig.getIndexMap();
         String config = "index";
         if (indexMap == null) {
             log.warn("索引未配置：{}", index);
-            map.put(config,"索引未配置") ;
+            map.put(config, "索引【" + index + "】未配置");
             return;
         }
         //
         IndexShardConfig indexShardConfig = indexMap.get(index);
         if (indexShardConfig == null) {
             log.warn("索引参数未配置：{}", index);
-            map.put(config,"索引参数未配置") ;
+            map.put(config, "索引【" + index + "】参数未配置");
             return;
         }
         //持久化信息
         JSONObject currentIndexInfo = getCurrentIndexInfo(indexShardConfig.getSwitchIndex());
         String switchIndex = "switchIndex";
-        if(currentIndexInfo==null){
-            map.put(switchIndex,"当前使用别名索引") ;
-        }else {
-            if(currentIndexInfo.containsKey(index)){
+        if (currentIndexInfo == null) {
+            map.put(switchIndex, "读【别名索引】");
+        } else {
+            if (currentIndexInfo.containsKey(index)) {
                 Integer indexStatus = currentIndexInfo.getInteger(index);
-                if(indexStatus.equals(MAIN_INDEX)){
-                    map.put(switchIndex,"当前持久化使用主索引") ;
-                }else{
-                    map.put(switchIndex,"当前持久化使用别名索引") ;
+                if (indexStatus.equals(MAIN_INDEX)) {
+                    map.put(switchIndex, "读【主索引】【持久化】");
+                } else {
+                    map.put(switchIndex, "读【别名索引】【持久化】");
                 }
-            }else {
-                map.put(switchIndex,"索引未初始化") ;
+            } else {
+                map.put(switchIndex, "索引【" + index + "】未初始化");
             }
         }
 
         //内存信息
         Integer ramStatus = CUR_INDEX_MAP.get(index);
         String ramIndex = "ramIndex";
-        if(ramStatus==null){
-            map.put(ramIndex,"当前索引内存信息不存在") ;
-        }else {
-            if(ramStatus.equals(MAIN_INDEX)){
-                map.put(ramIndex,"当前内存使用主索引") ;
-            }else{
-                map.put(ramIndex,"当前内存使用别名索引") ;
+        if (ramStatus == null) {
+            map.put(ramIndex, "索引【" + index + "】内存信息不存在");
+        } else {
+            if (ramStatus.equals(MAIN_INDEX)) {
+                map.put(ramIndex, "写【主索引】【内存】");
+            } else {
+                map.put(ramIndex, "写【别名索引】【内存】");
             }
         }
 
